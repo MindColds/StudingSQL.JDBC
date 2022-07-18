cc151c42f4b01487213abc83dec5f2b5fe02abfc
MindColds <105950494+MindColds@users.noreply.github.com>
2022-07-18 20:41:35 +0300

    1
    Add

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,6 +2,23 @@
 
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        Util.getConnection();
+
+        UserDao user = new UserDaoJDBCImpl();
+
+        user.createUsersTable();
+
+        user.saveUser("John", "Wik", (byte) 39);
+        user.saveUser("Zaur", "Tregulov", (byte) 32);
+        user.saveUser("Sergey", "Briliov", (byte) 41);
+        user.saveUser("Mason", "Capefell", (byte) 42);
+
+        user.removeUserById(4);
+
+        user.getAllUsers();
+
+        user.cleanUsersTable();
+
+        user.dropUsersTable();// реализуйте алгоритм здесь
     }
 }
